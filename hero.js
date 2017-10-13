ce613899f985e09ba774b4dbb958fc856df448f2
Raul Ruiz <rauletxailarena@gmail.com>
2017-10-13 14:06:28 +0100

    Add hero class

---
+++ hero.js
@@ -0,0 +1,9 @@
+var Hero = function(name, health, favouriteFood) {
+  this.name = name;
+  this.health = health;
+  this.favouriteFood = favouriteFood;
+  this.tasks = [];
+}
+
+
+module.exports = Hero
