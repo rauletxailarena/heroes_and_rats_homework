8e3e4d8225fff7dc8957e494403e09b8421677e6
Raul Ruiz <rauletxailarena@gmail.com>
2017-10-13 14:19:17 +0100

    Add three tests for hero

--- hero.js
+++ hero.js
@@ -3,7 +3,14 @@ var Hero = function(name, health, favouriteFood) {
   this.health = health;
   this.favouriteFood = favouriteFood;
   this.tasks = [];
-}
+  this.talk = function() {
+    return "Hello, my name is " + this.name;
+  }
+};
 
 
-module.exports = Hero
+module.exports = Hero;
+
+var a = new Hero("Asfas", "afasf", "Asfaf");
+
+console.log(a);
---
+++ specs/hero_spec.js
@@ -0,0 +1,24 @@
+var assert = require("assert")
+var Hero = require("../hero")
+
+describe("Hero", function() {
+
+
+  var hero;
+  beforeEach( function() {
+    hero = new Hero("Link", 100, "chicken");
+  });
+
+  it ("should have a name", function() {
+    assert.strictEqual(hero.name, "Link");
+  });
+
+  it ("should have a health value", function() {
+    assert.strictEqual(hero.health, 100);
+  });
+
+  it ("should have a favourite food", function() {
+    assert.strictEqual(hero.favouriteFood, "chicken");
+  });
+
+});
